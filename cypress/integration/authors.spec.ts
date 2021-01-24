5dc94d2b4b016072f76704efc82e9faef06e9b4e
Flávio Florentino <florentino.flavioweb@gmail.com>
2021-01-24 11:31:58 -0300

    test: 💍 added e2e spec for authors page

---
+++ cypress/integration/authors.spec.ts
@@ -0,0 +1,19 @@
+describe('when application finish loading routes', () => {
+  beforeEach(() => {
+    cy.visit('/authors');
+  });
+
+  it('should navigate to authors page and show the corret h2 title', () => {
+    const title = cy.get('h2');
+
+    expect(title.contains('Meus autores favoritos'));
+  });
+
+  it('should render a list of authors name formatted', () => {
+    const list = cy.get('ul');
+    list.get('li').contains('Santos, Lucas dos');
+    list.get('li').contains('Coelho, Paulo');
+    list.get('li').contains('Silva Neto, João');
+    list.get('li').contains('PEREIRA');
+  });
+});
